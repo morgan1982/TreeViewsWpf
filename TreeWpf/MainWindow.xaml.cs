9c3a3e85d1e3a579892c30027b2313bb2e171440
unknown <alexandrisanas@gmail.com>
2019-03-14 18:32:12 +0200

    init

---
+++ TreeWpf/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = drive;
+                item.Tag = drive;
+
+                item.Items.Add(null);
+
+                //Listen for an item being expanded
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            if(item.Items.Count != 1 && item.Items[0] != null)
+            {
+                return;
+            }
+
+            item.Items.Clear();
+
+            var fullPath = (string)item.Tag;
+
+
+        }
+    }
+}
