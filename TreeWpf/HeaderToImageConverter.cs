3a14e9e1e11f3904f5570a1ab7b476118366cd08
killerwhale <alexandrisanas@gmail.com>
2019-03-16 02:44:49 +0200

    init

---
+++ TreeWpf/HeaderToImageConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeWpf
+{
+    /// <summary>
+    /// Codnvertd a full path to a specific image type of a drive folder or file
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the full Path
+            var path = (string)value;
+
+            // if the path is null, ignore
+            if (path == null)
+                return null;
+
+            // Get the name of the file / folder
+            var name = MainWindow.GetFileFolderName(path);
+
+            // by default, we presume an image
+            var image = "Images/closed_folder.png";
+
+            // if name is blank, we presume it's a drive
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/closed_folder.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TreeWpf/MainWindow.xaml.cs
+++ TreeWpf/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TreeWpf
 {
@@ -28,8 +27,10 @@ namespace TreeWpf
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // Get every logical drive on the machine
             foreach(var drive in Directory.GetLogicalDrives())
             {
+                // Create a new item from it
                 var item = new TreeViewItem();
                 item.Header = drive;
                 item.Tag = drive;
@@ -42,11 +43,13 @@ namespace TreeWpf
                 FolderView.Items.Add(item);
             }
         }
-
+        #region Folder Expanded
         private void Folder_Expanded(object sender, RoutedEventArgs e)
         {
-            var item = (TreeViewItem)sender;
-            if(item.Items.Count != 1 && item.Items[0] != null)
+            var item = (TreeViewItem)sender; // cast it to a TreeViewItem
+
+            // if the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
             {
                 return;
             }
@@ -55,7 +58,107 @@ namespace TreeWpf
 
             var fullPath = (string)item.Tag;
 
+            #region Find Directories
+            // Create a blank list of directories
+            var directories = new List<string>();
 
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            // for Each directory
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    // set Header as folder name 
+                    Header = GetFileFolderName(directoryPath),
+                    // Tag as full path
+                    Tag = directoryPath
+
+                };
+
+                // add dummy item so we can expand the folder
+                subItem.Items.Add(null);
+
+                // handle the expanded items
+                subItem.Expanded += Folder_Expanded;
+
+                // Add this item to the parent
+                item.Items.Add(subItem);
+            });
+            #endregion
+
+            #region Get Files
+            // Create a blank list of files
+            var files = new List<string>();
+
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+
+                if(fs.Length > 0)
+                {
+                    files.AddRange(fs);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            // for Each directory
+            files.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    // set Header as file name 
+                    Header = GetFileFolderName(filePath),
+                    // Tag as full path
+                    Tag = filePath
+
+                };
+
+                // Add this item to the parent
+                item.Items.Add(subItem);
+            });
+        }
+        #endregion
+        #endregion
+        /// <summary>
+        /// Find the file or folder name from a full path
+        /// </summary>
+        /// <param name="path">The full Path</param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            // C:\something\a folder
+            if(string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            // Make all slasahes back slashes
+            var normalizedPath = path.Replace('/', '\\'); // uses single quotes cause \\ is actually a single char
+
+            // find the last backslash of the path
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+            // if there is no backslash return the path it self
+            if (lastIndex <= 0)
+                return path;
+
+            // return the name after the last backslash
+            return path.Substring(lastIndex + 1);
         }
     }
 }
